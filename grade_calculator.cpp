5a8c5d767825bb74448f41506a3b192b9c856b93
Richard Ly <rly758@gmail.com>
2020-06-06 16:36:09 -0700

    Synchronized spin boxes and sliders for homework

--- grade_calculator.cpp
+++ grade_calculator.cpp
@@ -13,3 +13,115 @@ grade_calculator::~grade_calculator()
     delete ui;
 }
 
+
+void grade_calculator::on_hw_slider_1_valueChanged(int value)
+{
+    if(value != hw_grade_1){
+        ui->hw_sbox_1->setValue(value);
+    }
+}
+
+void grade_calculator::on_hw_sbox_1_valueChanged(int value)
+{
+    if(value != hw_grade_1){
+        ui->hw_slider_1->setValue(value);
+    }
+}
+
+void grade_calculator::on_hw_slider_2_valueChanged(int value)
+{
+    if(value != hw_grade_2){
+        ui->hw_sbox_2->setValue(value);
+    }
+}
+
+void grade_calculator::on_hw_sbox_2_valueChanged(int value)
+{
+    if(value != hw_grade_2){
+        ui->hw_slider_2->setValue(value);
+    }
+}
+
+void grade_calculator::on_hw_slider_3_valueChanged(int value)
+{
+    if(value != hw_grade_3){
+        ui->hw_sbox_3->setValue(value);
+    }
+}
+
+void grade_calculator::on_hw_sbox_3_valueChanged(int value)
+{
+    if(value != hw_grade_3){
+        ui->hw_slider_3->setValue(value);
+    }
+}
+
+void grade_calculator::on_hw_slider_4_valueChanged(int value)
+{
+    if(value != hw_grade_4){
+        ui->hw_sbox_4->setValue(value);
+    }
+}
+
+void grade_calculator::on_hw_sbox_4_valueChanged(int value)
+{
+    if(value != hw_grade_4){
+        ui->hw_slider_4->setValue(value);
+    }
+}
+
+void grade_calculator::on_hw_slider_5_valueChanged(int value)
+{
+    if(value != hw_grade_5){
+        ui->hw_sbox_5->setValue(value);
+    }
+}
+
+void grade_calculator::on_hw_sbox_5_valueChanged(int value)
+{
+    if(value != hw_grade_5){
+        ui->hw_slider_5->setValue(value);
+    }
+}
+
+void grade_calculator::on_hw_slider_6_valueChanged(int value)
+{
+    if(value != hw_grade_6){
+        ui->hw_sbox_6->setValue(value);
+    }
+}
+
+void grade_calculator::on_hw_sbox_6_valueChanged(int value)
+{
+    if(value != hw_grade_6){
+        ui->hw_slider_6->setValue(value);
+    }
+}
+
+void grade_calculator::on_hw_slider_7_valueChanged(int value)
+{
+    if(value != hw_grade_7){
+        ui->hw_sbox_7->setValue(value);
+    }
+}
+
+void grade_calculator::on_hw_sbox_7_valueChanged(int value)
+{
+    if(value != hw_grade_7){
+        ui->hw_slider_7->setValue(value);
+    }
+}
+
+void grade_calculator::on_hw_slider_8_valueChanged(int value)
+{
+    if(value != hw_grade_8){
+        ui->hw_sbox_8->setValue(value);
+    }
+}
+
+void grade_calculator::on_hw_sbox_8_valueChanged(int value)
+{
+    if(value != hw_grade_8){
+        ui->hw_slider_8->setValue(value);
+    }
+}
--- grade_calculator.h
+++ grade_calculator.h
@@ -15,7 +15,64 @@ public:
     grade_calculator(QWidget *parent = nullptr);
     ~grade_calculator();
 
+    void set_hw_value(int value);
+    void set_midterm_value(int value);
+    void set_final_value(int value);
+public slots:
+    //void hw_value_changed(int value);
+    //void midterm_value_changed(int value);
+    //void final_value_changed(int value);
+    //void scheme_changed();
+
+signals:
+    //void equalize_slider_and_box(int value);
+
+private slots:
+    void on_hw_slider_1_valueChanged(int value);
+
+    void on_hw_sbox_1_valueChanged(int value);
+
+    void on_hw_slider_2_valueChanged(int value);
+
+    void on_hw_sbox_2_valueChanged(int value);
+
+    void on_hw_slider_3_valueChanged(int value);
+
+    void on_hw_sbox_3_valueChanged(int value);
+
+    void on_hw_slider_4_valueChanged(int value);
+
+    void on_hw_sbox_4_valueChanged(int value);
+
+    void on_hw_slider_5_valueChanged(int value);
+
+    void on_hw_sbox_5_valueChanged(int value);
+
+    void on_hw_slider_6_valueChanged(int value);
+
+    void on_hw_sbox_6_valueChanged(int value);
+
+    void on_hw_slider_7_valueChanged(int value);
+
+    void on_hw_sbox_7_valueChanged(int value);
+
+    void on_hw_slider_8_valueChanged(int value);
+
+    void on_hw_sbox_8_valueChanged(int value);
+
 private:
     Ui::grade_calculator *ui;
+    int hw_grade_1 = 0;
+    int hw_grade_2 = 0;
+    int hw_grade_3 = 0;
+    int hw_grade_4 = 0;
+    int hw_grade_5 = 0;
+    int hw_grade_6 = 0;
+    int hw_grade_7 = 0;
+    int hw_grade_8 = 0;
+    int midterm_grade_1 = 0;
+    int midterm_grade_2 = 0;
+    int final_grade = 0;
+    double overall_grade = 0.0;
 };
 #endif // GRADE_CALCULATOR_H
